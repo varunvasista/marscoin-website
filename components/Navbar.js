9311067e3bf3bde8f8711be5e91db72617e7d24e
varunvasista <varunvashista@engineer.com>
2024-03-11 13:56:57 +0530

    update logo fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,7 +11,7 @@ function Navbar() {
                     <div className={styles.logoSection}>
                         <Link href=""><a>
                             {/* <h1 className={styles.logoText}>Mars Coin</h1> */}
-                            <img src="/mars-white.png" alt="Mars Logo" className={styles.logoImage} />
+                            <img src="/mars.png" alt="Mars Logo" className={styles.logoImage} />
                         </a></Link>
                     </div>
                     <div className={styles.menuSection}>
